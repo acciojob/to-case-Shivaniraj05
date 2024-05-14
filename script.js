076ae35db3b1898b59f30edaaee2719cadf2596b
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2024-05-14 23:10:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function toCase(text) {
   // write your code here
+	if(!text || typeof text !== "string")
+		return "-";
+	
+	let upper = text.toUpperCase();
+	let lower = text.toLowerCase();
+
+	return lower+"-"+upper;
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+// const text = prompt("Enter text:");
+// alert(toCase(text));
