fb4bcaa4b56a9301fb6c0036b89d45bcc1d93e61
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2024-05-14 23:11:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,5 +11,5 @@ function toCase(text) {
 
 // DO not change the code below
 
-// const text = prompt("Enter text:");
-// alert(toCase(text));
+const text = prompt("Enter text:");
+alert(toCase(text));
